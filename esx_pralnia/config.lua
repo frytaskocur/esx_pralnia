bacfdca63fff07bd44e2347289185109f471b393
frytaskocur <81867007+frytaskocur@users.noreply.github.com>
2021-04-03 16:42:08 +0200

    Add files via upload

---
+++ esx_pralnia/config.lua
@@ -0,0 +1,15 @@
+Config					= {}
+Config.Locale = 'en'
+Config.Slice			= 8000
+Config.Percentage		= 90
+Config.MarkerSize   	= {x = 1.0, y = 1.0, z = 1.0}
+Config.openHours		= 1
+Config.closeHours		= 23
+Config.Blip 			= false
+Config.Hours 			= false
+Config.Menu				= false
+Config.Bonus			= {min = 0, max = 05}
+
+Config.Zones = {
+	{x = -314.22, y = -1355.70, z = 18.28,percent=80},
+}
---
+++ esx_pralnia/locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+	['blanchisseurblip'] 	= 'Pralnia ~p~pieniedzy',
+	['press_menu'] 			= 'Naciśnij ~INPUT_CONTEXT~ aby wyprać ~p~Brudne Pieniądze',
+	['Nothere'] 			= 'Wróć ~p~później ...',
+	['Whitening'] 			= '~p~Pranie ~w~w toku ...',
+	['Nocash'] 				= 'Nie masz już wystarczającej ilości ~p~brudnych pieniędzy, ~w~minimum :~r~ $',
+	['cash'] 				= 'Otrzymałeś : ~p~$ ',
+	['cash1'] 				= ' czyste ~p~pieniądze',
+	['Notification'] 		= 'Pralnia ~p~pieniedzy',
+	['wash'] 				= 'Wypierz swoje ~p~brudne pieniądze',
+	['wash_money_amount']   = 'Ilość do wyprania',
+	['invalid_amount']      = 'Nieprawidłowa ilość',
+}
+
